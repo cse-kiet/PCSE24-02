47cc00f8fcba050ba39f959780b9f4f345fe8986
rimanshusingh4 <rimanshusingh4@gmail.com>
2024-04-30 01:03:06 +0530

    First Commit

---
+++ frontend/src/pages/About/About.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import './About.css'
+
+export default function About() {
+    return (
+        <div className="about-container">
+            <h1 className="about-heading">About</h1>
+            <div className="about-main">
+                <p className="about-content">
+                    <h2> Major Project - Group-2</h2>
+                    This is an realtime AI based Yoga Trainer which detects your pose how well you are doing.
+                    I have developed this project, so people can use it.
+                    {/* 
+                    This AI first predicts keypoints or coordinates of different parts of the body(basically where
+                    they are present in an image) and then it use another classification model to classify the poses if 
+                    someone is doing a pose and if AI detects that pose more than 95% probability and then it will notify you are 
+                    doing correctly(by making virtual skeleton green). I have used Tensorflow pretrained Movenet Model To Predict the 
+                    Keypoints and building a neural network top of that which uses these coordinates and classify a yoga pose. */}
+
+                    I have trained the model in python because of tensorflowJS we can leverage the support of browser so I converted 
+                    the keras/tensorflow model to tensorflowJS.
+                </p>
+                {/* <div className="developer-info">
+                    <h4>About Developer</h4>
+                    
+                </div> */}
+            </div>
+        </div>
+    )
+}
